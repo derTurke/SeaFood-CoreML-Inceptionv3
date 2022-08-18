6a6e2d8fffbded7f5bf02b4a9c353e166b866c4b
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-08-19 01:11:47 +0300

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by GÜRHAN YUVARLAK on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
